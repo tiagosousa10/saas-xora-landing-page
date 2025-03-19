878987622f7ad936f8989c833a670761a7762b48
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-19 17:16:48 +0000

    button updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ import Button from '../components/Button'
 
 const Hero = () => {
   return (
-    <section className='relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-2 border-amber-300'>
+    <section className='relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 '>
       <Element name="hero"> 
          <div className='container'>
             <div className='relative z-2 max-w-512 max-lg:max-w-388' >
