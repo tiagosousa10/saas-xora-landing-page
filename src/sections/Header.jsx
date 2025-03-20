109801bfab5e84ab5bea6415d8063d0c8b66cce0
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-20 14:33:33 +0000

    Pricing section added, and first jsx buildings

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -2,18 +2,6 @@ import clsx from 'clsx'
 import React, { useEffect, useState } from 'react'
 import {Link as LinkScroll} from 'react-scroll'
 
-// NAVIGATION LINK to scroll over the page Element
-const NavLink = ({title}) => (
-   <LinkScroll
-      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
-      spy
-      smooth
-      to={title}
-      offset={-100}
-   >
-   {title}
-   </LinkScroll>
-)
 
 
 // ----- MAIN FUNCTION -----
@@ -35,8 +23,24 @@ const Header = () => {
    }, [])
 
 
+   // NAVIGATION LINK to scroll over the page Element
+   const NavLink = ({title}) => (
+      <LinkScroll
+         onClick={() => setIsOpen(false)}
+         className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+         activeClass='nav-active'
+         spy
+         smooth
+         to={title}
+         offset={-100}
+      >
+      {title}
+      </LinkScroll>
+   )
+
+
   return (
-    <header className={clsx('fixed top-0 left-0 z-50 w-full py-10', hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]')} >
+    <header className={clsx('fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4', hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]')} >
       <div className='container flex h-14 items-center max-lg:px-5'>
          <a href="" className='lg:hidden flex-1 cursor-pointer z-2'>
             <img src='/images/xora.svg' width={115} height={55} alt='logo' />
@@ -58,7 +62,7 @@ const Header = () => {
                   <li className='nav-logo'>
                      <LinkScroll
                         to='hero'
-                        offset={-100}
+                        offset={-200}
                         spy={true}
                         smooth 
                         className={clsx('max-lg:hidden transition-transform duration-500 cursor-pointer')}
