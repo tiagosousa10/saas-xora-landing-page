5fb7af8f8b240c8a0be6ff60cad448de824b5dd6
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-21 17:55:24 +0000

    faq updated and FaqItem added

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Element } from 'react-scroll'
+import { faq } from '../constants'
+import FaqItem from '../components/FaqItem'
 
 const Faq = () => {
+
+   const halfLength = Math.floor(faq.length/2)
+
   return (
     <section>
       <Element name="faq" className='relative'>
@@ -14,6 +19,25 @@ const Faq = () => {
                </h3>
                <p className='body-1 max-lg:max-w-sm'>You've got questions, we've got answers.</p>
             </div>
+
+            <div className='faq-line_after w-0.5 h-full absolute left-[calc(50%-1px)] top-0 -z-1 bg-s2'/>
+         </div>
+
+         <div className='faq-glow_before relative z-2 border-2 border-s2 bg-s1'>
+            <div className='container flex gap-10 max-lg:block'>
+               <div className='rounded-half absolute -top-10 left-[calc(50%-40px)] z-4 flex size-20 items-center justify-center border-2 border-s2 bg-s1'>
+                  <img src="/images/faq-logo.svg" alt="logo" className='size-1/2' />
+               </div>
+
+               <div className='relative flex-1 pt-24'>
+                  {faq.slice(0, halfLength).map((item,index) => (
+                    <FaqItem key={faq.id} item={item} index={index} />
+                  ))}
+
+               </div>
+
+            </div>
+
          </div>
 
       </Element>
