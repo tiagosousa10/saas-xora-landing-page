82f8baf701a0e5e89c4f4c60af30b565db081dec
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-24 12:18:15 +0000

    design the logos href jsx

--- src/sections/Download.jsx
+++ src/sections/Download.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Element } from 'react-scroll'
+import { links } from '../constants'
+import Marker from '../components/Marker'
 
 const Download = () => {
   return (
@@ -17,6 +19,53 @@ const Download = () => {
                   <p className='body-1 mb-10 max-w-md'>
                      Try it now for free on iOS, Android, PC, Web - whatever your flavor, we've got you covered
                   </p>
+
+                  <ul className='flex flex-wrap items-center gap-6'>
+                     {links.map(({id, url, icon}) => (
+                        <li 
+                           key={id} 
+                           className='download_tech-link download_tech-link_last-before download_tech-link_last-after'
+                        >
+                           <a 
+                              href={url} 
+                              className='size-22 download_tech-icon_before relative flex items-center justify-center rounded-half border-2 border-s3 bg-s1 transition-borderColor duration-500'
+                           >
+                              <span className='absolute -top-2 rotate-90'>
+                                 <Marker />
+                              </span>
+
+                              <img 
+                                 src="/images/lines.svg"  
+                                 alt="lines"           
+                                 className='absolute size-13/20 object-contain' 
+                              />
+                              <span className='download_tech-icon'>{icon}</span>
+                           </a>
+                        </li>
+                     ))}
+
+                  </ul>
+               </div>
+
+               <div className='mb-10 max-md:hidden'>
+                  <div 
+                     className='download_preview-before download_preview-after rounded-40 relative w-[955px] border-2 bg-s5 p-6'
+                  >
+                     <div className='relative rounded-3xl bg-s1 px-6 pb-6 pt-14'>
+                        <span className='download_preview-dot left-6 bg-p2'/>
+                        <span className='download_preview-dot left-11 bg-s3'/>
+                        <span className='download_preview-dot left-16 bg-p1/15'/>
+
+                        <img 
+                           src="/images/screen.jpg" 
+                           width={855} 
+                           height={655} 
+                           alt='screen' 
+                           className='rounded-xl' 
+                        />
+
+                     </div>
+                  </div>
                </div>
             </div>
          </div>
